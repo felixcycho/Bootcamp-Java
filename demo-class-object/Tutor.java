0daa0e9d4b033cbf648e54d6c7f387a84dd72dfd
felixcycho <felixcycho@yahoo.com.hk>
2025-08-13 17:22:24 +0800

    felixcycho@yahoo.com.hk

---
+++ demo-class-object/Tutor.java
@@ -0,0 +1,16 @@
+public class Tutor {
+  private String TA;
+  // Tutor 生產一件 object 時, 則該 object 的 TA 特徵, 是 unique 不會重覆.
+  // OOP (e.g. Java), 物件導向語言, 則用作一個容器, 將 objects 的所有 attributes, 有足夠空間包含.
+  // 當 user 需要從某一 attribute, 去搜尋一個 object 時, OOP 則自動且快速地 access 容器, 搜尋 object.
+  // e.g. [] 是 array, 
+  // Developer 學習 OOP, 是因為 OOP 可不斷製造 container, 以 contain data.
+  // class 可比 container / file, class 可 store object / save data.
+  // runtime 運行時間
+  // main
+  // public class 只是一個 container, 不會 operate.
+  // 
+  // e.g. to create a 撲克牌 game, 需要兩個 classes, i.e. character, 
+
+  
+}
